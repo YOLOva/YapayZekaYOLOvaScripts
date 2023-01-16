7e3da1bb7cbf95bd9628c9bef9b32e3380f68c5d
mr-aydemir <aydemir_emre65@hotmail.com>
2023-01-16 13:40:02 +0300

    update

--- label_mapper/label_mapper.py
+++ label_mapper/label_mapper.py
@@ -2,45 +2,75 @@
 import os
 from pathlib import Path
 import sys
+import argparse
+
+def run(
+        labels_folder="",
+        output_folder="",
+        label_map_file="./label_map_uyz_2023.txt",
+        ):
+
+    if labels_folder == '':
+        print("labels_folder parametresi eksik.")
+        exit()
+
+    if not Path(labels_folder).is_dir():
+        print("hatalı labels_folder dizin")
+        exit()
+
+    if output_folder == "":
+        output_folder = labels_folder+"_new"
+        
+    elif not Path(output_folder).is_dir():
+        print("hatalı output_folder dizin")
+        exit()
+    
+    if not Path(label_map_file).is_file():
+        print("hatalı label_map_file dizin")
+        exit()
+
+    # txt dosyasından map listesi çeker
+    with open(label_map_file, "r") as file:
+        lines = file.readlines()
+
+    class_map = []
+    for line in lines:
+        class_map.append(line.split()[0])
+    print(class_map)
+
+    # label dosyalarını okuyup sınıflarını mape göre değiştirir
+    labels_file_names = os.listdir(labels_folder)
+    for label_file_name in labels_file_names:
+        if label_file_name == 'classes.txt':
+            continue
+        label_path = f"{labels_folder}/{label_file_name}"
+        lines = []
+        with open(label_path, "r") as label_file:
+            data = label_file.readlines()
+            for i, line in enumerate(data):
+                cls = line.split()[0]
+                if class_map[int(cls)] == "-1":
+                    continue
+                lines.append(line.replace(cls, class_map[int(cls)], 1))
+
+        Path(labels_folder+"_new").mkdir(exist_ok=True)
+        with open(f"{output_folder}/{label_file_name}", 'w') as file:
+            file.writelines(lines)
 
 
-if len(sys.argv) < 2:
-    print("eksik parametre")
-    exit()
 
-labels_folder = sys.argv[1]  # "D:/Teknofest/YOLOVA/Veriseti/Visdrone2019Det"
-if not Path(labels_folder).is_dir():
-    print("hatalı dizin")
-    exit()
-"""labels_folder = "D:/Teknofest/YOLOVA/Veriseti/New/spat_data/labels_old" #input
-output_folder = "D:/Teknofest/YOLOVA/Veriseti/New/spat_data/labels" #output"""
-label_map_file = "./label_map_uyz_2023.txt"
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--labels_folder', type=str, help='label klasörü dizini', required=True)
+    parser.add_argument('--output_folder', default="", type=str, help='çıktı klasör dizini')
+    parser.add_argument('--label_map_file', type=str, default="./label_map_uyz_2023.txt")
+    opt = parser.parse_args()
+    return opt
 
-# txt dosyasından map listesi çeker
-with open(label_map_file, "r") as file:
-    lines = file.readlines()
+def main(opt):
+    run(**vars(opt))
 
-class_map = []
-for line in lines:
-    class_map.append(line.split()[0])
-print(class_map)
 
-# label dosyalarını okuyup sınıflarını mape göre değiştirir
-labels_file_names = os.listdir(labels_folder)
-for label_file_name in labels_file_names:
-    if label_file_name == 'classes.txt':
-        continue
-    label_path = f"{labels_folder}/{label_file_name}"
-    lines = []
-    with open(label_path, "r") as label_file:
-        data = label_file.readlines()
-        for i, line in enumerate(data):
-            cls = line.split()[0]
-            if class_map[int(cls)] == "-1":
-                continue
-            lines.append(line.replace(cls, class_map[int(cls)], 1))
-    output_folder = labels_folder+"_new"
-
-    Path(labels_folder+"_new").mkdir(exist_ok=True)
-    with open(f"{output_folder}/{label_file_name}", 'w') as file:
-        file.writelines(lines)
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
--- label_mapper/readme.md
+++ label_mapper/readme.md
@@ -1,10 +1,14 @@
 # label mapper
 ## kullanım
-python label_mapper.py "[label_klasörü_dizini]"
+python label_mapper.py --labels_folder [label_klasörü_dizini] --label_map_file [label_map_dosyası_dizini] --output_folder [çıktı klasör dizini]
 
 
 
 çıktı [label_klasörü_dizini]_new şeklinde oluşacaktır.
+buradaki --labels_folder parametresi girilmesi zorunludur. 
+
+Onun dışındaki parametrelerin girilmesi zorunlu değildir.
+
 
 txt dosyaları içerisine verisetinin classes.txt dosyasını düzenleyerek her sınıfın yeni class id karşılığını satır başına belirtilir.
 
