3432c5988b1fed3964b82114ec65b9108842b65b
mr-aydemir <aydemir_emre65@hotmail.com>
2023-02-13 15:43:27 +0300

    update

--- analyze/class_count.py
+++ analyze/class_count.py
@@ -1,28 +1,29 @@
 from pathlib import Path
 
 
-classes_txt=r"classes.txt"
+classes_txt = r"classes.txt"
 
 with open(classes_txt, "r") as f:
-    class_list= [x.replace("\n", "") for x in f.readlines()]
+    class_list = [x.replace("\n", "") for x in f.readlines()]
 
 
-dataset_folder_path= r"D:\Teknofest\YOLOVA\Veriseti\KesisenYol"
+dataset_folder_path = r"D:\Teknofest\YOLOVA\Veriseti\KesisenYol"
 
 
-class_count_list=[0]*len(class_list)
+class_count_list = {id: {"name": name, "count": 0}
+                    for id, name in enumerate(class_list)}
 
-for txt_path in list(Path(dataset_folder_path).glob("**/*.txt")):
-    txt_path=str(txt_path)
+for txt_path in Path(dataset_folder_path).glob("**/*.txt"):
+    txt_path = str(txt_path)
     if "classes.txt" in txt_path:
         continue
 
     with open(txt_path) as f:
-        lines=f.readlines()
+        lines = f.readlines()
         for line in lines:
-            cols=line.split()
-            cls=int(cols[0])
-            class_count_list[cls]+=1
+            cols = line.split()
+            cls = int(cols[0])
+            class_count_list[cls]["count"] += 1
 
-for i, count in  enumerate(class_count_list):
-    print(f"{count} sayıda {class_list[i]}")
+for key, data in class_count_list.items():
+    print(f"{data['name']}: {data['count']}")
